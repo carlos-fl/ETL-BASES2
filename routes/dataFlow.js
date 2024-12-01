40628c3a3a0d787ee972cf27bbabe5c6f6fd4123
carlos flores <carlosftru@gmail.com>
2024-11-30 19:32:36 -0600

    run project

--- routes/dataFlow.js
+++ routes/dataFlow.js
@@ -6,9 +6,4 @@ export const dataFlowRouter = Router();
 dataFlowRouter.get('/dataFlow', DataFlow.redirectToBoard);
 dataFlowRouter.post('/connect', DataFlow.connection);
 dataFlowRouter.post('/tableNames', DataFlow.getTableNames);
-<<<<<<< HEAD
 dataFlowRouter.post('/destTableMetadata', DataFlow.getDestinationMetadata);
-
-=======
-dataFlowRouter.post('/destTableMetadata', DataFlow.getDestinationMetadata);
->>>>>>> dev/run
